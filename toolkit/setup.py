977cc5747f4a7d5299d14dc227ad0c36e23c03a2
Benjamin Hamon <hamon.benjamin@gmail.com>
2022-10-15 18:59:21 +0200

    Add python package

---
+++ toolkit/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+def run_setup():
+	parameters = {
+		"version": "1.0.0",
+		"author": "Benjamin Hamon",
+
+		"name": "bhamon-book-distribution-toolkit",
+		"description": "Toolkit for creating book distributions",
+
+		"packages": [
+			"bhamon_book_distribution_toolkit",
+		],
+
+		"python_requires": "~= 3.7",
+	}
+
+	parameters.update({
+		
+	})
+
+	setuptools.setup(**parameters)
+
+
+run_setup()

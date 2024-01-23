a48e25bc7769b341a50166b131b51e32ed36e639
Benjamin Hamon <development@benjaminhamon.com>
2024-01-23 12:48:56 +0100

    Add document operation to convert styles

--- Sources/toolkit/benjaminhamon_book_distribution_toolkit/documents/document_operations.py
+++ Sources/toolkit/benjaminhamon_book_distribution_toolkit/documents/document_operations.py
@@ -1,4 +1,4 @@
-from typing import Iterator
+from typing import Dict, Iterator, Optional
 
 from benjaminhamon_book_distribution_toolkit import convert_helpers
 from benjaminhamon_book_distribution_toolkit.documents.document_element import DocumentElement
@@ -14,3 +14,15 @@ def enumerate_all_elements(element: DocumentElement) -> Iterator[DocumentElement
 def generate_section_file_name(section_title: str, section_index: int, section_count: int) -> str:
     title = str(section_index + 1).rjust(len(str(section_count)), "0") + " - " + section_title
     return convert_helpers.sanitize_for_file_name(title)
+
+
+def convert_styles(root: DocumentElement, style_map: Dict[str,Optional[str]]) -> None:
+    for element in enumerate_all_elements(root):
+        new_style_collection = []
+        for style in element.style_collection:
+            if style not in style_map:
+                raise ValueError("Unexpected style: '%s'" % style)
+            style_converted = style_map[style]
+            if style_converted is not None:
+                new_style_collection.append(style_converted)
+        element.style_collection = new_style_collection

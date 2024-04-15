a258049c2ceb843e5ae18e61cbd1663b082af60b
Benjamin Hamon <development@benjaminhamon.com>
2024-04-15 14:23:40 +0200

    Add script to split a fodt file

---
+++ Sources/scripts/benjaminhamon_document_manipulation_scripts/split_odt.py
@@ -0,0 +1,78 @@
+# cspell:words fodt lxml
+
+import argparse
+import os
+import shutil
+from typing import Optional
+
+import lxml.etree
+
+from benjaminhamon_document_manipulation_scripts import script_helpers
+from benjaminhamon_document_manipulation_toolkit.open_document.odt_reader import OdtReader
+from benjaminhamon_document_manipulation_toolkit.open_document.odt_writer import OdtWriter
+
+
+def main() -> None:
+    arguments = parse_arguments()
+
+    script_helpers.configure_logging(arguments.verbosity)
+
+    split_odt(
+        source_file_path = os.path.normpath(arguments.source),
+        destination_directory = os.path.normpath(arguments.destination),
+        template_file_path = os.path.normpath(arguments.template) if arguments.template is not None else None,
+        overwrite = arguments.overwrite)
+
+
+def parse_arguments() -> argparse.Namespace:
+    argument_parser = argparse.ArgumentParser(description = "Split an odt file into one file per section.")
+    argument_parser.add_argument("--source", required = True, metavar = "<path>", help = "path to the odt or fodt file to use as the source")
+    argument_parser.add_argument("--destination", required = True, metavar = "<path>", help = "path to the directory where to create the new fodt files")
+    argument_parser.add_argument("--template", metavar = "<path>", help = "path to the fodt file to use as the template")
+    argument_parser.add_argument("--overwrite", action = "store_true", help = "overwrite the destination if it exists")
+
+    argument_parser.add_argument("--verbosity", choices = script_helpers.all_logging_levels, default = "info", type = str.lower,
+        metavar = "<level>", help = "set the logging level (%s)" % ", ".join(script_helpers.all_logging_levels))
+
+    arguments = argument_parser.parse_args()
+
+    return arguments
+
+
+def split_odt(
+        source_file_path: str,
+        destination_directory: str,
+        template_file_path: Optional[str] = None,
+        overwrite: bool = False,
+        simulate: bool = False) -> None:
+
+    if os.path.exists(destination_directory):
+        if not overwrite:
+            raise RuntimeError("Destination already exists")
+
+    xml_parser = lxml.etree.XMLParser(encoding = "utf-8", remove_blank_text = True)
+    odt_reader = OdtReader(xml_parser)
+    odt_writer = OdtWriter(xml_parser)
+
+    odt_content = _read_source(odt_reader, source_file_path)
+    document_content = odt_reader.read_content(odt_content)
+    document_comments = odt_reader.read_comments(odt_content)
+
+    if not simulate:
+        if os.path.exists(destination_directory):
+            shutil.rmtree(destination_directory)
+        os.makedirs(destination_directory)
+
+    odt_writer.write_as_many_documents(
+        destination_directory, document_content, document_comments,
+        template_file_path = template_file_path, flat_odt = True, simulate = simulate)
+
+
+def _read_source(odt_reader: OdtReader, source_file_path: str) -> bytes:
+    if source_file_path.endswith(".fodt"):
+        return odt_reader.read_fodt(source_file_path)
+    return odt_reader.read_odt(source_file_path, "content.xml")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Tests/scripts/benjaminhamon_document_manipulation_scripts_tests/test_split_odt.py
@@ -0,0 +1,101 @@
+# cspell:words fodt opendocument
+
+import os
+
+from benjaminhamon_document_manipulation_scripts.split_odt import split_odt
+
+
+def test_split_odt(tmpdir):
+    workspace_directory = os.path.join(tmpdir, "Workspace")
+    source_file_path = os.path.join(workspace_directory, "FullText.fodt")
+    output_directory = os.path.join(workspace_directory, "SectionsAsOdt")
+
+    _setup_workspace(workspace_directory)
+
+    split_odt(
+        source_file_path = source_file_path,
+        destination_directory = output_directory,
+        simulate = False,
+    )
+
+    _assert_output(workspace_directory)
+
+
+def test_split_odt_with_simulate(tmpdir):
+    workspace_directory = os.path.join(tmpdir, "Workspace")
+    source_file_path = os.path.join(workspace_directory, "FullText.fodt")
+    output_directory = os.path.join(workspace_directory, "SectionsAsOdt")
+
+    _setup_workspace(workspace_directory)
+
+    split_odt(
+        source_file_path = source_file_path,
+        destination_directory = output_directory,
+        simulate = True,
+    )
+
+    assert not os.path.exists(output_directory)
+
+
+def _setup_workspace(workspace_directory: str) -> None:
+    source_file_path = os.path.join(workspace_directory, "FullText.fodt")
+
+    fodt_data = """
+<?xml version="1.0" encoding="utf-8"?>
+<office:document xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0">
+  <office:body>
+    <office:text>
+      <text:h text:style-name="section-heading">Chapter 1</text:h>
+      <text:p text:style-name="section-paragraph">Chapter 1 paragraph 1</text:p>
+      <text:p text:style-name="section-paragraph">Chapter 1 paragraph 2</text:p>
+      <text:p text:style-name="section-paragraph">Chapter 1 paragraph 3</text:p>
+      <text:h text:style-name="section-heading">Chapter 2</text:h>
+      <text:p text:style-name="section-paragraph">Chapter 2 paragraph 1</text:p>
+      <text:p text:style-name="section-paragraph">Chapter 2 paragraph 2</text:p>
+      <text:p text:style-name="section-paragraph">Chapter 2 paragraph 3</text:p>
+      <text:h text:style-name="section-heading">Chapter 3</text:h>
+      <text:p text:style-name="section-paragraph">Chapter 3 paragraph 1</text:p>
+      <text:p text:style-name="section-paragraph">Chapter 3 paragraph 2</text:p>
+      <text:p text:style-name="section-paragraph">Chapter 3 paragraph 3</text:p>
+    </office:text>
+  </office:body>
+</office:document>
+"""
+
+    fodt_data = fodt_data.lstrip()
+
+    os.makedirs(workspace_directory)
+    with open(source_file_path, mode = "w", encoding = "utf-8") as source_file:
+        source_file.write(fodt_data)
+
+
+def _assert_output(workspace_directory: str) -> None:
+    output_directory = os.path.join(workspace_directory, "SectionsAsOdt")
+
+    files_in_destination = os.listdir(output_directory)
+    files_in_destination_expected = [ "1 - Chapter 1.fodt", "2 - Chapter 2.fodt", "3 - Chapter 3.fodt" ]
+
+    assert files_in_destination == files_in_destination_expected
+
+    for chapter_number in [ 1, 2, 3 ]:
+        fodt_file_path = os.path.join(output_directory, "{chapter_number} - Chapter {chapter_number}.fodt".format(chapter_number = chapter_number))
+        with open(fodt_file_path, mode = "r", encoding = "utf-8") as fodt_file:
+            fodt_data = fodt_file.read()
+
+        fodt_data_expected = """
+<?xml version="1.0" encoding="utf-8"?>
+<office:document xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0">
+  <office:body>
+    <office:text>
+      <text:h text:style-name="section-heading"><text:span>Chapter {chapter_number}</text:span></text:h>
+      <text:p text:style-name="section-paragraph"><text:span>Chapter {chapter_number} paragraph 1</text:span></text:p>
+      <text:p text:style-name="section-paragraph"><text:span>Chapter {chapter_number} paragraph 2</text:span></text:p>
+      <text:p text:style-name="section-paragraph"><text:span>Chapter {chapter_number} paragraph 3</text:span></text:p>
+    </office:text>
+  </office:body>
+</office:document>
+"""
+
+        fodt_data_expected = fodt_data_expected.lstrip().format(chapter_number = chapter_number)
+
+        assert fodt_data == fodt_data_expected

6875b37d7b210d52c15b7dc5d1529295813cdcf0
Benjamin Hamon <development@benjaminhamon.com>
2024-04-19 13:27:26 +0200

    Write comments in odt files

--- Sources/toolkit/benjaminhamon_document_manipulation_toolkit/open_document/odt_builder.py
+++ Sources/toolkit/benjaminhamon_document_manipulation_toolkit/open_document/odt_builder.py
@@ -1,14 +1,17 @@
 # cspell:words lxml nsmap
 
-from typing import List, Optional
+from typing import Dict, List, Optional
 
 import lxml.etree
 
+from benjaminhamon_document_manipulation_toolkit.documents.document_comment import DocumentComment
 from benjaminhamon_document_manipulation_toolkit.documents.heading_element import HeadingElement
 from benjaminhamon_document_manipulation_toolkit.documents.paragraph_element import ParagraphElement
 from benjaminhamon_document_manipulation_toolkit.documents.root_element import RootElement
 from benjaminhamon_document_manipulation_toolkit.documents.section_element import SectionElement
 from benjaminhamon_document_manipulation_toolkit.documents.text_element import TextElement
+from benjaminhamon_document_manipulation_toolkit.documents.text_region_end_element import TextRegionEndElement
+from benjaminhamon_document_manipulation_toolkit.documents.text_region_start_element import TextRegionStartElement
 from benjaminhamon_document_manipulation_toolkit.open_document import odt_operations
 
 
@@ -23,33 +26,45 @@ class OdtBuilder:
         return self._xml_document
 
 
-    def add_content(self, root_element: RootElement) -> None:
+    def add_content(self, root_element: RootElement, comment_collection: Dict[str,DocumentComment]) -> None:
         for section_element in root_element.enumerate_sections():
-            self.add_section(section_element)
+            self.add_section(section_element, comment_collection)
 
 
-    def add_section(self, section_element: SectionElement) -> None:
-        self.add_heading(section_element.get_heading())
+    def add_section(self, section_element: SectionElement, comment_collection: Dict[str,DocumentComment]) -> None:
+        self.add_heading(section_element.get_heading(), comment_collection)
 
         for paragraph in section_element.enumerate_paragraphs():
-            self.add_paragraph(paragraph)
+            self.add_paragraph(paragraph, comment_collection)
 
         for subsection in section_element.enumerate_subsections():
-            self.add_section(subsection)
+            self.add_section(subsection, comment_collection)
 
 
-    def add_heading(self, heading_element: HeadingElement) -> None:
+    def add_heading(self, heading_element: HeadingElement, comment_collection: Dict[str,DocumentComment]) -> None:
         heading_as_xml = self._create_text_xml_element(None, "h", heading_element.style_collection, None)
 
-        for text_element in heading_element.enumerate_text():
-            self.add_text(heading_as_xml, text_element)
+        for child in heading_element.children:
+            if isinstance(child, TextElement):
+                self.add_text(heading_as_xml, child)
+            if isinstance(child, TextRegionStartElement):
+                comment = self._get_comment_for_region(child, comment_collection)
+                self._create_annotation_xml_element(heading_as_xml, child, comment)
+            if isinstance(child, TextRegionEndElement):
+                self._create_annotation_end_xml_element(heading_as_xml, child)
 
 
-    def add_paragraph(self, paragraph_element: ParagraphElement) -> None:
+    def add_paragraph(self, paragraph_element: ParagraphElement, comment_collection: Dict[str,DocumentComment]) -> None:
         paragraph_as_xml = self._create_text_xml_element(None, "p", paragraph_element.style_collection, None)
 
-        for text_element in paragraph_element.enumerate_text():
-            self.add_text(paragraph_as_xml, text_element)
+        for child in paragraph_element.children:
+            if isinstance(child, TextElement):
+                self.add_text(paragraph_as_xml, child)
+            if isinstance(child, TextRegionStartElement):
+                comment = self._get_comment_for_region(child, comment_collection)
+                self._create_annotation_xml_element(paragraph_as_xml, child, comment)
+            if isinstance(child, TextRegionEndElement):
+                self._create_annotation_end_xml_element(paragraph_as_xml, child)
 
 
     def add_text(self, paragraph_as_xml: lxml.etree._Element, text_element: TextElement) -> None:
@@ -61,28 +76,66 @@ class OdtBuilder:
     def _create_text_xml_element(self,
             parent: Optional[lxml.etree._Element], tag: str, style_collection: List[str], text: Optional[str]) -> lxml.etree._Element:
 
-        body_as_xml = odt_operations.get_body_text_element(self._xml_document)
-        if parent is None:
-            parent = body_as_xml
+        namespaces = self._xml_document.getroot().nsmap
 
-        attributes = {}
+        if parent is None:
+            parent = odt_operations.get_body_text_element(self._xml_document)
+
+        attributes: Dict[str,str] = {}
         if len(style_collection) > 1:
             raise ValueError("Too many styles")
         if len(style_collection) == 1:
-            attributes[lxml.etree.QName(body_as_xml.nsmap["text"], "style-name")] = style_collection[0]
+            attributes[str(lxml.etree.QName(namespaces["text"], "style-name"))] = style_collection[0]
 
-        element = lxml.etree.SubElement(parent, lxml.etree.QName(body_as_xml.nsmap["text"], tag), attrib = attributes)
+        element = lxml.etree.SubElement(parent, lxml.etree.QName(namespaces["text"], tag), attrib = attributes)
         element.text = text
 
         return element
 
 
     def _create_line_break_element(self, parent: Optional[lxml.etree._Element]) -> lxml.etree._Element:
-        body_as_xml = odt_operations.get_body_text_element(self._xml_document)
-        if parent is None:
-            parent = body_as_xml
+        namespaces = self._xml_document.getroot().nsmap
 
-        tag = lxml.etree.QName(body_as_xml.nsmap["text"], "line-break")
+        if parent is None:
+            parent = odt_operations.get_body_text_element(self._xml_document)
+
+        tag = lxml.etree.QName(namespaces["text"], "line-break")
         element = lxml.etree.SubElement(parent, tag)
 
         return element
+
+
+    def _create_annotation_xml_element(self,
+            paragraph_as_xml: lxml.etree._Element, region_start_element: TextRegionStartElement, comment: DocumentComment) -> None:
+
+        namespaces = self._xml_document.getroot().nsmap
+
+        if region_start_element.identifier is None:
+            raise ValueError("Region element must have an identifier")
+
+        attributes: Dict[str,str] = {}
+        attributes[str(lxml.etree.QName(namespaces["office"], "name"))] = region_start_element.identifier
+        comment_as_xml = lxml.etree.SubElement(paragraph_as_xml, lxml.etree.QName(namespaces["office"], "annotation"), attrib = attributes)
+
+        lxml.etree.SubElement(comment_as_xml, lxml.etree.QName(namespaces["dc"], "creator")).text = comment.author
+        lxml.etree.SubElement(comment_as_xml, lxml.etree.QName(namespaces["dc"], "date")).text = comment.date.isoformat()
+
+        for text_paragraph in comment.text.splitlines():
+            self._create_text_xml_element(comment_as_xml, "p", [], text_paragraph)
+
+
+    def _create_annotation_end_xml_element(self, paragraph_as_xml: lxml.etree._Element, region_end_element: TextRegionEndElement) -> None:
+        namespaces = self._xml_document.getroot().nsmap
+
+        if region_end_element.identifier is None:
+            raise ValueError("Region element must have an identifier")
+
+        attributes: Dict[str,str] = {}
+        attributes[str(lxml.etree.QName(namespaces["office"], "name"))] = region_end_element.identifier
+        lxml.etree.SubElement(paragraph_as_xml, lxml.etree.QName(namespaces["office"], "annotation-end"), attrib = attributes)
+
+
+    def _get_comment_for_region(self, region_start_element: TextRegionStartElement, comment_collection: Dict[str,DocumentComment]) -> DocumentComment:
+        if region_start_element.identifier is None:
+            raise ValueError("Region element must have an identifier")
+        return comment_collection[region_start_element.identifier]
--- Tests/toolkit/benjaminhamon_document_manipulation_toolkit_tests/open_document/test_odt_writer.py
+++ Tests/toolkit/benjaminhamon_document_manipulation_toolkit_tests/open_document/test_odt_writer.py
@@ -2,17 +2,21 @@
 
 """ Unit tests for OdtWriter """
 
+import datetime
 import os
 import zipfile
 
 import lxml.etree
 
 from benjaminhamon_document_manipulation_toolkit.documents import document_element_factory
+from benjaminhamon_document_manipulation_toolkit.documents.document_comment import DocumentComment
 from benjaminhamon_document_manipulation_toolkit.documents.root_element import RootElement
+from benjaminhamon_document_manipulation_toolkit.documents.text_region_end_element import TextRegionEndElement
+from benjaminhamon_document_manipulation_toolkit.documents.text_region_start_element import TextRegionStartElement
 from benjaminhamon_document_manipulation_toolkit.open_document.odt_writer import OdtWriter
 
 
-def create_document() -> RootElement:
+def create_generic_document() -> RootElement:
     document = RootElement()
 
     document.children.append(
@@ -42,11 +46,11 @@ def test_write_as_single_document_to_fodt(tmpdir):
     xml_parser = lxml.etree.XMLParser(encoding = "utf-8", remove_blank_text = True)
     odt_writer = OdtWriter(xml_parser)
 
-    document = create_document()
+    document = create_generic_document()
     fodt_file_path = os.path.join(tmpdir, "Working", "MyDocument.fodt")
 
     os.makedirs(os.path.dirname(fodt_file_path))
-    odt_writer.write_as_single_document(fodt_file_path, document, flat_odt = True, simulate = False)
+    odt_writer.write_as_single_document(fodt_file_path, document, [], flat_odt = True, simulate = False)
 
     assert os.path.exists(fodt_file_path)
 
@@ -74,15 +78,73 @@ def test_write_as_single_document_to_fodt(tmpdir):
     assert actual_content == expected_content
 
 
-def test_write_as_single_document_to_odt(tmpdir):
+def test_write_as_single_document_to_fodt_with_comments(tmpdir):
+
+    def create_document() -> RootElement:
+        document = RootElement()
+
+        document.children.append(
+            document_element_factory.create_section(
+                heading = "The Section",
+                text = [
+                    [ "Before the comment.", "Inside the comment." ],
+                    [ "Inside the comment again.", "After the comment." ]
+                ],
+            )
+        )
+
+        return document
+
+    def insert_comment(document: RootElement) -> DocumentComment:
+        document.children[0].children[1].children.insert(1, TextRegionStartElement("__Annotation__123"))
+        document.children[0].children[2].children.insert(1, TextRegionEndElement("__Annotation__123"))
+
+        return DocumentComment("__Annotation__123", "Benjamin Hamon", datetime.datetime(2020, 1, 1), "Some comment.\nMore text in the comment.")
+
+    def get_expected_fodt() -> str:
+        expected_content = """
+<?xml version="1.0" encoding="utf-8"?>
+<office:document xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0">
+  <office:body>
+    <office:text>
+      <text:h><text:span>The Section</text:span></text:h>
+      <text:p><text:span>Before the comment.</text:span><office:annotation office:name="__Annotation__123"><dc:creator>Benjamin Hamon</dc:creator><dc:date>2020-01-01T00:00:00</dc:date><text:p>Some comment.</text:p><text:p>More text in the comment.</text:p></office:annotation><text:span>Inside the comment.</text:span></text:p>
+      <text:p><text:span>Inside the comment again.</text:span><office:annotation-end office:name="__Annotation__123"/><text:span>After the comment.</text:span></text:p>
+    </office:text>
+  </office:body>
+</office:document>
+"""
+
+        return expected_content.lstrip()
+
     xml_parser = lxml.etree.XMLParser(encoding = "utf-8", remove_blank_text = True)
     odt_writer = OdtWriter(xml_parser)
 
     document = create_document()
+    comment = insert_comment(document)
+    fodt_file_path = os.path.join(tmpdir, "Working", "MyDocument.fodt")
+
+    os.makedirs(os.path.dirname(fodt_file_path))
+    odt_writer.write_as_single_document(fodt_file_path, document, [ comment ], flat_odt = True, simulate = False)
+
+    assert os.path.exists(fodt_file_path)
+
+    expected_content = get_expected_fodt()
+    with open(fodt_file_path, mode = "r", encoding = "utf-8") as odt_file:
+        actual_content = odt_file.read()
+
+    assert actual_content == expected_content
+
+
+def test_write_as_single_document_to_odt(tmpdir):
+    xml_parser = lxml.etree.XMLParser(encoding = "utf-8", remove_blank_text = True)
+    odt_writer = OdtWriter(xml_parser)
+
+    document = create_generic_document()
     odt_file_path = os.path.join(tmpdir, "Working", "MyDocument.odt")
 
     os.makedirs(os.path.dirname(odt_file_path))
-    odt_writer.write_as_single_document(odt_file_path, document, flat_odt = False, simulate = False)
+    odt_writer.write_as_single_document(odt_file_path, document, [], flat_odt = False, simulate = False)
 
     assert os.path.exists(odt_file_path)
 
@@ -116,8 +178,8 @@ def test_write_as_single_document_to_fodt_with_simulate(tmpdir):
 
     fodt_file_path = os.path.join(tmpdir, "Working", "MyDocument.fodt")
 
-    document = create_document()
-    odt_writer.write_as_single_document(fodt_file_path, document, flat_odt = True, simulate = True)
+    document = create_generic_document()
+    odt_writer.write_as_single_document(fodt_file_path, document, [], flat_odt = True, simulate = True)
 
     assert not os.path.exists(fodt_file_path)
 
@@ -128,8 +190,8 @@ def test_write_as_single_document_to_odt_with_simulate(tmpdir):
 
     odt_file_path = os.path.join(tmpdir, "Working", "MyDocument.odt")
 
-    document = create_document()
-    odt_writer.write_as_single_document(odt_file_path, document, flat_odt = False, simulate = True)
+    document = create_generic_document()
+    odt_writer.write_as_single_document(odt_file_path, document, [], flat_odt = False, simulate = True)
 
     assert not os.path.exists(odt_file_path)
 
@@ -138,11 +200,11 @@ def test_write_as_many_documents_to_fodt(tmpdir):
     xml_parser = lxml.etree.XMLParser(encoding = "utf-8", remove_blank_text = True)
     odt_writer = OdtWriter(xml_parser)
 
-    document = create_document()
+    document = create_generic_document()
     odt_directory = os.path.join(tmpdir, "Working", "MyDocument")
 
     os.makedirs(odt_directory)
-    odt_writer.write_as_many_documents(odt_directory, document, flat_odt = True, simulate = False)
+    odt_writer.write_as_many_documents(odt_directory, document, [], flat_odt = True, simulate = False)
 
     assert len(os.listdir(odt_directory)) == 2
 
@@ -199,11 +261,11 @@ def test_write_as_many_documents_to_odt(tmpdir):
     xml_parser = lxml.etree.XMLParser(encoding = "utf-8", remove_blank_text = True)
     odt_writer = OdtWriter(xml_parser)
 
-    document = create_document()
+    document = create_generic_document()
     odt_directory = os.path.join(tmpdir, "Working", "MyDocument")
 
     os.makedirs(odt_directory)
-    odt_writer.write_as_many_documents(odt_directory, document, flat_odt = False, simulate = False)
+    odt_writer.write_as_many_documents(odt_directory, document, [], flat_odt = False, simulate = False)
 
     assert len(os.listdir(odt_directory)) == 2
 
@@ -260,10 +322,10 @@ def test_write_as_many_documents_to_fodt_with_simulate(tmpdir):
     xml_parser = lxml.etree.XMLParser(encoding = "utf-8", remove_blank_text = True)
     odt_writer = OdtWriter(xml_parser)
 
-    document = create_document()
+    document = create_generic_document()
     odt_directory = os.path.join(tmpdir, "Working", "MyDocument")
 
-    odt_writer.write_as_many_documents(odt_directory, document, flat_odt = True, simulate = True)
+    odt_writer.write_as_many_documents(odt_directory, document, [], flat_odt = True, simulate = True)
 
     assert not os.path.exists(odt_directory)
 
@@ -272,9 +334,9 @@ def test_write_as_many_documents_to_odt_with_simulate(tmpdir):
     xml_parser = lxml.etree.XMLParser(encoding = "utf-8", remove_blank_text = True)
     odt_writer = OdtWriter(xml_parser)
 
-    document = create_document()
+    document = create_generic_document()
     odt_directory = os.path.join(tmpdir, "Working", "MyDocument")
 
-    odt_writer.write_as_many_documents(odt_directory, document, flat_odt = False, simulate = True)
+    odt_writer.write_as_many_documents(odt_directory, document, [], flat_odt = False, simulate = True)
 
     assert not os.path.exists(odt_directory)

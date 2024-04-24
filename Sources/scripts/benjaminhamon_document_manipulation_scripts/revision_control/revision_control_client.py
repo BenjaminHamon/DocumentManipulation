82b59c7f0e5b04c584f930226d90576ba56a1576
Benjamin Hamon <development@benjaminhamon.com>
2024-04-24 20:18:48 +0200

    Add script to generate an information xhtml file

---
+++ Sources/scripts/benjaminhamon_document_manipulation_scripts/revision_control/revision_control_client.py
@@ -0,0 +1,36 @@
+import abc
+import datetime
+from typing import Optional
+
+
+class RevisionControlClient(abc.ABC):
+
+
+    @abc.abstractmethod
+    def get_current_revision(self) -> str:
+        pass
+
+
+    @abc.abstractmethod
+    def get_current_branch(self) -> Optional[str]:
+        pass
+
+
+    @abc.abstractmethod
+    def try_resolve_revision(self, reference: str) -> Optional[str]:
+        pass
+
+
+    @abc.abstractmethod
+    def resolve_revision(self, reference: str) -> str:
+        pass
+
+
+    @abc.abstractmethod
+    def get_revision_date(self, revision: str) -> datetime.datetime:
+        pass
+
+
+    @abc.abstractmethod
+    def convert_revision_to_short(self, revision: str) -> str:
+        pass
